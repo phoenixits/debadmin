8e37c4dc30c6335317aa6dff2d6eec4212c5d8cb
phoenixits <bounty@phoenixit.org>
2015-03-21 17:56:54 +0100

    Update debadmin.py

--- debadmin.py
+++ debadmin.py
@@ -7,47 +7,183 @@ import time
 os.system("clear")
 time.sleep(1)
 
+def Header():
+   print("#" * 70)
+   os.system("whoami")
+   print("Server:")
+   os.system("hostname -I")
+   print("Uptime:")
+   os.system("uptime")
+   print("Status:")
+   os.system("service apache2 status")
+   print("#" * 70)
+
+def Upgrade():
+   print "####################################"
+   print "# Running Upgrade Process          #"
+   print "####################################"
+   print "Please wait..."
+   time.sleep(2)
+   os.system("clear")
+   print("Running package updates")
+   time.sleep(2)
+   os.system("apt-get update")
+   os.system("clear")
+   time.sleep(2)
+   print "Completed..."
+   time.sleep(2)
+   os.system("clear")
+   print "Upgrading Installed Packages"
+   time.sleep(2)
+   os.system("apt-get upgrade")
+   os.system("clear")
+   time.sleep(2)
+   print "Completed..."
+   time.sleep(2)
+   os.system("clear")
+   print("Upgrading Linux Distribution")
+   time.sleep(2)
+   os.system("apt-get dist-upgrade")
+   os.system("clear")
+   time.sleep(2)
+   print("Completed...")
+   time.sleep(2)
+   os.system("clear")
+   time.sleep(2)
+   print("Cleaning Disk Space")
+   time.sleep(2)
+   os.system("apt-get clean")
+   os.system("apt-get clean all")
+   time.sleep(2)
+   print("Completed...")
+   time.sleep(2)
+   os.system("clear")
+   print("Process complete...")
+   time.sleep(2)
+   
+
+def MemUsage():
+   print "##########################"
+   print "# Free Memory            #"
+   print "##########################"
+   time.sleep(2)
+   os.system("free -m")
+   print("Process complete...")
+   time.sleep(2)
+
+def SpeedTest():
+   print "###########################"
+   print "# Performing Speed Test   #"
+   print "###########################"
+   time.sleep(2)
+   os.system("wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip | grep 'in'")
+   print("Process complete...")
+   time.sleep(2)
+
+def InstallFB():
+   print "##########################"
+   print "# Installing Fail2Ban    #"
+   print "##########################"
+   time.sleep(2)
+   os.system("apt-get install fail2ban")
+   print("Process complete...")
+   time.sleep(2)
+
+def checkBanLog():
+   print "##########################"
+   print "# Checking Fail2Ban Log  #"
+   print "##########################"
+   time.sleep(2)
+   os.system("cat /var/log/fail2ban.log")
+   time.sleep(2)
+   print("Process complete...")
+   time.sleep(2)
+
+def memoryUsage():
+   print "##########################"
+   print "# Checking Memory Usage  #"
+   print "##########################"
+   time.sleep(2)
+   os.system("clear")
+   print "#################################################"
+   print "# Memory Usage                                  #"
+   print "#                                               #"
+   os.system("egrep --color 'Mem|Cache|Swap' /proc/meminfo")
+   print "#################################################"
+
+def installApache():
+   print("#" * 70)
+   print("Installing Apache2 and PHP")
+   time.sleep(2)
+   os.system("clear")
+   print("Installing Apache2 now...")
+   time.sleep(2)
+   os.system("apt-get install apache2")
+   print("Completed...")
+   time.sleep(2)
+   os.system("clear")
+   print("Installing PHP5 now...")
+   os.system("apt-get install php5 php-pear php5-mysql")
+   print("Completed...")
+   time.sleep(2)
+   os.system("clear")
+   print("Restarting Apache2...")
+   time.sleep(2)
+   os.system("service apache2 restart")
+   print("Completed...")
+   time.sleep(2)
+   os.system("clear")
+   print("Checking PHP Version...")
+   time.sleep(2)
+   os.system("php -v")
+   time.sleep(2)
+   
+
+
+Header()        
 ans=True
 while ans:
    print("""
    1. Install Fail2Ban
    2. Check Fail2Ban log
    3. Run Speed Test
-   4. Perform Updates
-   5. Perform Upgrades
-   6. Upgrade Distribution
-   7. Clean Up Diskspace
-   8. Quit
+   4. Perform Updates & Upgrades & Clean Disk Space 
+   5. Memory Usage and Free Memory
+   6. Install Apache2 & PHP5
+   7. Quit
    """)
    ans=raw_input("What would you like to do? ")
    if ans=="1":
-      print("\nInstalling Fail2Ban")
-      time.sleep(2)
-      os.system("apt-get install fail2ban")
+      InstallFB()
       os.system("clear")
+      Header()
    elif ans=="2":
+      checkBanLog()
       time.sleep(2)
-      os.system("cat /var/log/fail2ban.log")
+      os.system("clear")
+      Header()
    elif ans=="3":
-      print("\nRunning Speed Test")
+      SpeedTest()
       time.sleep(2)
-      os.system("wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip | grep 'in'")
+      os.system("clear")
+      Header()
    elif ans=="4":
-      print("\nPerforming Updates")
+      Upgrade()
       time.sleep(2)
-      os.system("apt-get update")
+      os.system("clear")
+      Header()
    elif ans=="5":
-      print("\nPerforming Upgrades")
+      memoryUsage()
+      MemUsage()
       time.sleep(2)
-      os.system("apt-get upgrade")
+      os.system("clear")
+      Header()
    elif ans=="6":
-      print("\nPerforming Distribution Upgrades")
+      installApache()
       time.sleep(2)
-      os.system("apt-get dist-upgrade")
+      os.system("clear")
+      Header()
    elif ans=="7":
-      time.sleep(2)
-      os.system("apt-get clean")
-   elif ans=="8":
       print("\nQuitting program...")
       ans = None
    else:
